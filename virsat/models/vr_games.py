80f74df28b4375dc3828c5e603feceef689f92d7
= <=>
2023-06-19 14:09:26 +0400

    [FIX] add game status computations, added tracking to selected fields

--- virsat/models/vr_games.py
+++ virsat/models/vr_games.py
@@ -5,13 +5,16 @@ from datetime import datetime
 
 class VrGame(models.Model):
     _name = "vr.games"
+    _inherit = ['mail.thread']
     _description = "VR Game"
 
-    name = fields.Char()
-    code = fields.Char()
+    name = fields.Char(string="Training Module")
+    code = fields.Char(tracking=True, required=True)
     description = fields.Text()
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     levels_count = fields.Integer(compute='compute_level_count')
+    compute_status = fields.Selection([('percentage', 'Percentage'), ('score', 'Score')], default='percentage', tracking=True)
+    compute_status_qty = fields.Float(string="Compute Quantity", default=100, tracking=True)
 
     _sql_constraints = [
         ('code_company_uniq', 'unique(code,company_id)', 'Game code must be unique per company.'),
@@ -39,7 +42,7 @@ class VrGameLevels(models.Model):
     name = fields.Char(required=True)
     code = fields.Char(required=True)
     passing_score = fields.Integer(default=1)
-    game_id = fields.Many2one("vr.games")
+    game_id = fields.Many2one("vr.games", string="Training Module")
 
     _sql_constraints = [
         ('code_game_uniq', 'unique(code,game_id)', 'Level code must be unique per game.'),
@@ -48,20 +51,22 @@ class VrGameLevels(models.Model):
 
 class VrGameSessions(models.Model):
     _name = "vr.game.sessions"
+    _inherit = ['mail.thread']
     _description = "VR Game Sessions"
     _order = 'id desc'
 
-    name = fields.Char(string='PIN')
+    name = fields.Char(string='PIN', tracking=True)
     company_id = fields.Many2one('res.company', compute='get_company', store=True)
-    company_code = fields.Char()
+    company_code = fields.Char(tracking=True)
     vr_trainee_id = fields.Many2one('vr.trainee', compute='get_vr_trainee', string="Trainee", store=True)
     session_start_str = fields.Char(string="Session Start")
-    session_start = fields.Datetime(compute="compute_session_start", store=True)
+    session_start = fields.Datetime(compute="compute_session_start", store=True, tracking=True)
     session_end_str = fields.Char(string="Session End")
-    session_end = fields.Datetime(compute="compute_session_end", store=True)
-    game_result_ids = fields.One2many('vr.game.result', 'game_session_id')
+    session_end = fields.Datetime(compute="compute_session_end", store=True, tracking=True)
+    game_id = fields.Many2one('vr.games', string="Training Module", tracking=True)
+    game_result_ids = fields.One2many('vr.game.result', 'game_session_id', readonly=True)
     vr_mail_id = fields.Many2one('virsat.vr.mails')
-    status = fields.Selection([("passed", "Passed"), ('failed', 'Failed')], compute="compute_status", store=True)
+    status = fields.Selection([("passed", "Passed"), ('failed', 'Failed')], compute="compute_status", store=True, tracking=True)
 
     @api.depends('session_start_str')
     def compute_session_start(self):
@@ -88,8 +93,24 @@ class VrGameSessions(models.Model):
     @api.depends('game_result_ids.remark')
     def compute_status(self):
         for res in self:
-            remark = res.game_result_ids.mapped('remark')
-            res.status = 'failed' if any(s in ('Incorrect', 'Failed') for s in remark) else 'passed'
+            # remark = res.game_result_ids.mapped('remark')
+            # res.status = 'failed' if any(s in ('Incorrect', 'Failed') for s in remark) else 'passed'
+
+            passing = res.game_id.compute_status_qty
+            if res.game_id.compute_status == 'score':
+                total_score = 0
+                level_scores = {}
+                for g in res.game_result_ids:
+                    if g.level_code not in level_scores:
+                        level_scores[g.level_code] = []
+                    level_scores[g.level_code] += [g.score]
+                for k, v in level_scores.items():
+                    total_score += max(v)
+                res.status = 'passed' if total_score >= passing else 'failed'
+            else:
+                unique_violation = res.game_result_ids.mapped('violation')
+                correct = res.game_result_ids.filtered(lambda g: g.remark.lower() in ('passed', 'correct')).mapped('remark')
+                res.status = 'passed' if (len(correct) / len(set(unique_violation))) * 100 >= passing else 'failed'
 
     @api.depends('company_code')
     def get_company(self):
